d41b20a0e26d20b5798df0b6637570ba213b1263
wakanak2 <w.koba.0214@gmail.com>
2020-06-06 18:36:51 +0900

    [update]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,13 +2,13 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    redirect_to user_path(user)
+    redirect_back(fallback_location: root_path)
   end  
 
   def destroy
     @user = User.find(params[:relationship][:followed_id])
     current_user.unfollow!(@user)
-    redirect_to user_path(user)
-  end  
+    redirect_back(fallback_location: root_path)
+  end
   
 end
